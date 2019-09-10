05826e8dbb6067c03b89abae9ab6f3a0f3627bfe
oyekunle-mark <oyekunlemac@gmail.com>
2019-09-10 19:00:06 +0100

    Connects to the mongo server when app starts

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const connect = require('./database/connect');
+
 const server = require('./api/server');
 
 const PORT = 5000;
 
-server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+connect('mongodb://localhost:27017/noteCRUD')
+  .then(() =>
+    server.listen(PORT, () => console.log(`Server listening on port ${PORT}`)),
+  )
+  .catch(err => console.error(err));
