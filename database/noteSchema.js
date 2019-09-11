4c23d74c2bac6a448ede40b0b8bdf42803a3dba1
oyekunle-mark <oyekunlemac@gmail.com>
2019-09-11 16:46:55 +0100

    Added a timestamp feature to the schema

--- database/noteSchema.js
+++ database/noteSchema.js
@@ -2,15 +2,20 @@ const mongoose = require('mongoose');
 
 mongoose.set('useCreateIndex', true);
 
-module.exports = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    unique: true,
+module.exports = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    body: {
+      type: String,
+      required: true,
+      minlength: 15,
+    },
   },
-  body: {
-    type: String,
-    required: true,
-    minlength: 15,
+  {
+    timestamps: true,
   },
-});
+);
