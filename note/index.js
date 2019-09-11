9413cbfda9996c15899cbdc98c4d49143bf00432
oyekunle-mark <oyekunlemac@gmail.com>
2019-09-11 16:20:39 +0100

    paths for edit and get single note

--- note/index.js
+++ note/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-const { getNotes, addNote } = require('./noteController');
+const { getNotes, addNote, getANote, editNote } = require('./noteController');
 
 router.get('/notes', getNotes);
+router.get('/notes/:id', getANote);
 router.post('/note', addNote);
+router.put('/notes/:id', editNote);
 
 module.exports = router;
