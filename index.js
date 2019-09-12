5c26094ecbb3a26664ea7947e32c02c0d94d4181
oyekunle-mark <oyekunlemac@gmail.com>
2019-09-12 10:39:40 +0100

    Used the debug library for logging

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const connect = require('./database/connect');
+const debug = require('debug')('noteCRUD');
 
+const connect = require('./database/connect');
 const server = require('./api/server');
 
 const PORT = 5000;
 
 connect('mongodb://localhost:27017/noteCRUD')
   .then(() =>
-    server.listen(PORT, () => console.log(`Server listening on port ${PORT}`)),
+    server.listen(PORT, () => debug(`Server listening on port ${PORT}`)),
   )
-  .catch(err => console.error(err));
+  .catch(err => debug(err));
